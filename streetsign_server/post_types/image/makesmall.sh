8ead6da75a4819e626784514942468e4d083ce90
James Wait <james@wait.ac>
2026-06-24 10:04:39 +1000

    Fix makesmall.sh: use magick with fallback to convert (IMv7 deprecation)

--- streetsign_server/post_types/image/makesmall.sh
+++ streetsign_server/post_types/image/makesmall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-convert "$1" -resize 1280x\> "$1"
+if command -v magick &> /dev/null; then
+    magick "$1" -resize 1280x\> "$1"
+else
+    convert "$1" -resize 1280x\> "$1"
+fi
 
 exit $?
